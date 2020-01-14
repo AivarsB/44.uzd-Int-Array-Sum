724c648a38e83a7da68802814b8344b7a979b081
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:22:49 +0200

    Add project files.

---
+++ 44.uzd Int Array Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _44.uzd_Int_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 26, 8, 9, 10, 32, 18, 24 };
+
+            var sum = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number > 10)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
+// Definēt int tipa masīvu ar 8 elementiem. Sasummēt tikai tos elementus, kuru vērtība ir lielāka par 10.
